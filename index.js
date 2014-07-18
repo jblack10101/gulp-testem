f8aa8aec0327c8741408c20960bd09df584341bb
Jordan Black <jblack@scrippsnetworks.com>
2014-07-18 15:39:17 -0400

    started playing around with CI mode and running multiple testem files

--- index.js
+++ index.js
@@ -12,78 +12,87 @@ var spawn = require('child_process').spawn;
 var testemApi = require('testem');
 
 var testemPlugin = function(options) {
-  var child;
-  var stream;
-  var files = [];
+    var child;
+    var stream;
+    var files = [];
 
-  options = extend({
-    action: 'run'
-  }, options);
+    options = extend({
+        action: 'run'
+    }, options);
 
-  if (options.configFile) {
-    options.configFile = path.resolve(options.configFile);
-  }
-
-  function done(code) {
-    // Stop the server if it's running
-    if (child) {
-      child.kill();
+    if (options.configFile) {
+        options.configFile = path.resolve(options.configFile);
+    }
+    if (!options.mode) {
+        options = extend({
+            mode: "dev"
+        }, options);
     }
 
-    // End the stream if it exists
-    if (stream) {
-      stream.emit('end');
-    }
-  }
+    function done(code) {
+        // Stop the server if it's running
+        if (child) {
+            gutil.log("killed child");
+            child.kill();
+        }
 
-  function startTestemServer() {
-    gutil.log('Starting Testem server...');
+        // End the stream if it exists
+        if (stream) {
+            gutil.log("ended stream");
+            stream.emit('end');
+        }
 
-    // Start the server
-    child = spawn(
-      'node',
-      [
-        path.join(__dirname, 'lib', 'background.js'),
-        JSON.stringify(options)
-      ],
-      {
-        stdio: 'inherit'
-      }
-    );
-
-    // Cleanup when the child process exits
-    child.on('exit', function() {
-      // gutil.log('Testem child process ended');
-      done();
-    });
-  }
-
-  function queueFile(file) {
-    if (file) {
-      // gutil.log('Queueing file '+file.path);
-      files.push(file.path);
-    }
-    else {
-      stream.emit('error', new Error('Got undefined file'));
-    }
-  }
-
-  function endStream() {
-    // Override files if they were piped
-    // This doesn't work with the runner, but works fine with singleRun and autoWatch
-    if (files.length) {
-      options.files = files;
+        process.exit(0);
     }
 
-    // Start the server
-    // If options.singleRun: Server starts, tests run, task completes
-    // If options.background: Server starts, tests run, files watched
-    startTestemServer();
-  }
+    function startTestemServer() {
+        gutil.log('Starting Testem server...');
 
-  stream = es.through(queueFile, endStream);
+        // Start the server
+        child = spawn(
+            'node',
+            [
+                path.join(__dirname, 'lib', 'background.js'),
+                JSON.stringify(options)
+            ],
+            {
+                stdio: 'inherit'
+            }
+        ).pipe;
 
-  return stream;
+        // Cleanup when the child process exits
+        child.on('exit', function() {
+            gutil.log('Testem child process ended');
+            done();
+        });
+    }
+
+    function queueFile(file) {
+        if (file) {
+            gutil.log('Queueing file ' + file);
+            files.push(file);
+        }
+        else {
+            stream.emit('error', new Error('Got undefined file'));
+        }
+    }
+
+    function endStream() {
+        // Override files if they were piped
+        // This doesn't work with the runner, but works fine with singleRun and autoWatch
+        if (files.length) {
+            options.files = files;
+        }
+
+        // Start the server
+        // If options.singleRun: Server starts, tests run, task completes
+        // If options.background: Server starts, tests run, files watched
+        startTestemServer();
+    }
+
+    stream = es.through(queueFile, endStream);
+
+    return stream;
 };
 
 module.exports = testemPlugin;
